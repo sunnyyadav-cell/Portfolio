5b40a30938a4705bc67b56de07d316fd0bb480c8
Sunny Kumar <sunnyjee9234640116@gmail.com>
2026-05-12 17:42:05 -0400

    Depo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './Components/Navbar.jsx'
-import Hero from './Components/Hero.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
