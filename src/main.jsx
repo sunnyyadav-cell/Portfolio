ed5dcec8196de2dbf2854eaaba6c03f4fa9577da
Sunny Kumar <sunnyjee9234640116@gmail.com>
2026-05-12 17:29:18 -0400

    Depo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Navbar from './Components/navbar.jsx'
+import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 
 
